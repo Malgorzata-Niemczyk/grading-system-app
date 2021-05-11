3f2b38486a58cd27c1896e58119a8047eb18043b
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-11 16:47:05 +0200

    generate files for the grades-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { GradesListComponent } from './components/grades-list/grades-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    GradesListComponent
   ],
   imports: [
     BrowserModule,
