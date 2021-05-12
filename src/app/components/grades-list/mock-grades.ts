ecb61555f2b073e90be37bdecd7a806f5d7e00a6
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-12 14:13:18 +0200

    add indents to the mock-grades.ts file

--- src/app/components/grades-list/mock-grades.ts
+++ src/app/components/grades-list/mock-grades.ts
@@ -6,8 +6,28 @@ interface gradesList {
 }
 
 export const GRADES: gradesList[] = [
-    {id: "1", minPercentage: 0, maxPercentage: 24, symbolicGrade: "F"},
-    {id: "2", minPercentage: 25, maxPercentage: 49, symbolicGrade: "E"},
-    {id: "3", minPercentage: 50, maxPercentage: 74, symbolicGrade: "D"},
-    {id: "4", minPercentage: 75, maxPercentage: 99, symbolicGrade: "C"},
+    {
+        id: "1", 
+        minPercentage: 0, 
+        maxPercentage: 24, 
+        symbolicGrade: "F"
+    },
+    {
+        id: "2", 
+        minPercentage: 25, 
+        maxPercentage: 49, 
+        symbolicGrade: "E"
+    },
+    {
+        id: "3", 
+        minPercentage: 50, 
+        maxPercentage: 74, 
+        symbolicGrade: "D"
+    },
+    {
+        id: "4", 
+        minPercentage: 75, 
+        maxPercentage: 99, 
+        symbolicGrade: "C"
+    },
 ]
