b198ad4bf37538840b1803436861aa58ee642536
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-12 15:35:29 +0200

    delete unnecessary imports

--- src/app/services/grades.service.ts
+++ src/app/services/grades.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { gradesList } from '../../app/components/grades-list/mock-grades';
 
 @Injectable({
