40a39ced8d9a4112ee0c6926e9bb24103ffb3ee0
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-12 21:13:39 +0200

    create UI service with the property to show the add new grade form

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
+  private showAddGradeForm: boolean = false;
+  private subject = new Subject<any>();
 
   constructor() { }
+
+  toggleAddGradeForm(): void {
+    this.showAddGradeForm = !this.showAddGradeForm;
+    this.subject.next(this.showAddGradeForm);
+  }
+
+  onToggle(): Observable<any> {
+    return this.subject.asObservable();
+  }
 }
