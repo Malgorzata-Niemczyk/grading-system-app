62a34d156daa9561cf12cb469903ea6c0c13c38a
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-13 01:56:28 +0200

    delete unnecessary code and update the content of the text showed in the add button

--- src/app/components/buttons/add-button/add-button.component.ts
+++ src/app/components/buttons/add-button/add-button.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-button',
@@ -7,7 +6,6 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./add-button.component.scss']
 })
 export class AddButtonComponent implements OnInit {
-  faPlus = faPlus;
   @Input() text: string;
   @Output() btnClick = new EventEmitter();
 
