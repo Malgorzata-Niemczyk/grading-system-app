fb713feb0fc564cad7186f2f646bc654e5278c6c
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-16 01:28:34 +0200

    add content to and style the edit-form component

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-edit-form',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-form.component.scss']
 })
 export class EditFormComponent implements OnInit {
+  faSave = faSave;
 
   constructor() { }
 
