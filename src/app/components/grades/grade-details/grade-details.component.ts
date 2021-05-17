3ce78318cbb66502e4a01486909aee16448b0327
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-17 19:57:55 +0200

    set up form validation to the editable fields on the grade details page

--- src/app/components/grades/grade-details/grade-details.component.ts
+++ src/app/components/grades/grade-details/grade-details.component.ts
@@ -5,6 +5,7 @@ import { GradesService } from '../../../services/grades.service';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { gradesList } from '../grades-list/mock-grades';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -17,6 +18,28 @@ export class GradeDetailsComponent implements OnInit {
   faChevronCircleLeft = faChevronCircleLeft;
 
   @Input() grade: gradesList;
+
+  // properties for the form fields
+  minPercentage: number;
+  maxPercentage: number;
+  symbolicGrade: string;
+  descriptiveGrade: string;
+
+  //properties for the form option fields
+  grades: string[] = [
+    'A - Excellent',
+    'B - Very Good',
+    'C - Good',
+    'D - Satisfactory',
+    'E - Poor',
+    'F - Failing'
+  ];
+
+  exform = new FormGroup({
+    'minPercentage': new FormControl(null, [Validators.min(0), Validators.max(100), Validators.required]),
+    'maxPercentage': new FormControl(null, [Validators.min(0), Validators.max(100), Validators.required]),
+    'symbolicGrade': new FormControl(null, Validators.required)
+  });
 
   constructor(
     private route: ActivatedRoute,
