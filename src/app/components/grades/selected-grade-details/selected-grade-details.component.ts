a037a2434942e160b5d8fe9512c89cc77df5b731
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-20 13:17:26 +0200

    add the content and styles for the SelectedGradeDetails component

--- src/app/components/grades/selected-grade-details/selected-grade-details.component.ts
+++ src/app/components/grades/selected-grade-details/selected-grade-details.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { gradesList } from '../grades-list/mock-grades';
 
 @Component({
   selector: 'app-selected-grade-details',
   templateUrl: './selected-grade-details.component.html',
   styleUrls: ['./selected-grade-details.component.scss']
 })
+
 export class SelectedGradeDetailsComponent implements OnInit {
+  faEdit = faEdit;
+  @Input() grade: gradesList;
 
   constructor() { }
 
